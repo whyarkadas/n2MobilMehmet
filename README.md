16f09e8593979c220984f77a2e946c699d0acba0
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2018-08-31 07:18:35 +0300

    renew

--- README.md
+++
@@ -1,8 +0,0 @@
-# n2MobilMehmet
-N2Mobil Mehmet
-
-python manage.py runserver
-
-http://127.0.0.1:8000/mehmet/
-
-
--- mehmet/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms  
-from mehmet.models import Vehicle  
-class VehicleForm(forms.ModelForm):  
-    class Meta:  
-        model = Vehicle  
-        fields = "__all__"  
